cf76ff0d193b327116f4e6b95c2ce686a1df319a
heartalborada <heartalborada@outlook.com>
2023-07-15 13:37:56 +0800

    feat: add i.pximg.net proxy

--- routes/imageProxy.js
+++ routes/imageProxy.js
@@ -2,13 +2,25 @@ let express = require('express');
 const {createProxyMiddleware} = require("http-proxy-middleware");
 let router = express.Router();
 
-const proxyMiddleware = createProxyMiddleware({
-    target: 'https://i.pximg.net',
+const proxyMiddleware = process.env.NODE_ENV === 'development' ?  createProxyMiddleware( {
+    target: 'https://i.pixiv.re',
     changeOrigin: true,
+    pathRewrite: {
+        '^/pixivImage': '/'
+    },
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.170',
         'Referer': 'https://www.pixiv.net/'
-    }
+    },
+}) : createProxyMiddleware( {
+    target: 'https://i.pximg.net',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/pixivImage': '/'
+    },
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.170',
+    },
 });
 
 router.use('/', proxyMiddleware);
